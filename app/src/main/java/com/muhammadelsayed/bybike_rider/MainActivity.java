8fd8cfe62353e7b8a5907f0d0c1a898371602338
MuhammadElsayed96 <muhammadelsayed96@yahoo.com>
2018-03-05 02:14:46 +0200

    Initial commit

---
+++ app/src/main/java/com/muhammadelsayed/bybike_rider/MainActivity.java
@@ -0,0 +1,13 @@
+package com.muhammadelsayed.bybike_rider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
